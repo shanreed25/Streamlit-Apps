052f59d0e74ed93eff6d9cd7c86e4c5fb353934f
Shanreed25 <tralainereed@gmail.com>
2026-01-12 21:29:40 -0800

    adding canva and smithAI

---
+++ ai/pages/SmithAI.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from modules.navigation import SidebarNav
+
+def intro():
+    st.title("Smith AI")
+    st.markdown("""
+    #### :rainbow[Smith AI is a virtual receptionist and client engagement platform that utilizes artificial intelligence to provide businesses with 24/7 customer support and lead management. 
+    # It offers services such as answering calls, scheduling appointments, qualifying leads, and providing information to enhance customer interactions and streamline business operations.]
+                """)
+    
+    st.markdown("Smith AI: https://smith.ai/")
+    st.write("First Month Free with code: INNOVISION")
+
+
+
+    # https://www.monetizedmarketingtraining.com/ai-marketing-summit-2025-recordings-may150211705512687/ai-marketing-summit-2025-recordings-may/ai-image-summit-recording3ef61a987a3d/
